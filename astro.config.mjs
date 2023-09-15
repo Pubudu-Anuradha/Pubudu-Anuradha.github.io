59f552a259df3dde642cf19d4e7316f32ca63179
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-09-15 23:39:32 +0530

    eslint,prettier and blog test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pubudu-anuradha.github.io/",
-  integrations: [mdx()]
+  site: 'https://pubudu-anuradha.github.io/',
+  integrations: [mdx()],
 });
